d638c955f3e5d9c8a0bbc94165c7838962b8c9fb
Mungwa Agu <mungwaagu@gmail>
2023-06-27 12:13:28 +0100

    Updated Betty Style in _printf_handler

--- _printf.c
+++ _printf.c
@@ -1,19 +1,19 @@
 #include "main.h"
 
 /**
- * _printf - A function that produces output according to a format.
- * @format: A character string
- * Return: number of characters printed out
- */
+* _printf - A function that produces output according to a format.
+* @format: A character string
+* Return: number of characters printed out
+*/
 
 int _printf(const char *format, ...)
 {
-        va_list vl;
-        int printed_chars = 0;
+	va_list vl;
+	int printed_chars = 0;
 
-        va_start(vl, format);
-        printed_chars = _printf_handler(format, vl);
-        va_end(vl);
+	va_start(vl, format);
+	printed_chars = _printf_handler(format, vl);
+	va_end(vl);
 
-        return (printed_chars);
+	return (printed_chars);
 }
--- _printf_handler.c
+++ _printf_handler.c
@@ -1,1 +1,50 @@
+#include :se"main.h"
 
+/**
+* _printf_handler - A function to handle format specifiers for _printf
+* @format: A character string
+* @args: A variable argument list
+* Return: number of characters printed from variable arguments
+* to character string
+*/
+
+int _printf_handler(const char *format, va_list args)
+{
+	int printed_chars = 0;
+
+	while (*format)
+	{
+		if (*format == '%')
+		{
+			format++;
+			if (*format == 'c')
+			{
+				_putchar(va_arg(args, int));
+				printed_chars = 1;
+			}
+			else if (*format == 's')
+			{
+				const char *s = va_arg(args, const char *);
+
+				while (*s != '\0')
+				{
+					_putchar(*s++);
+					printed_chars += 1;
+				}
+			}
+			else if (*format == '%')
+			{
+				_putchar('%');
+				_putchar('%');
+				printed_chars = 1;
+			}
+
+		}
+		else
+		{
+			_putchar(*format);
+		}
+		format++
+	}
+	return (printed_chars);
+}
